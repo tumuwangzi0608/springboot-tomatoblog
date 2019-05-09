600b69a8bbd1c9a190d77c63aadb469fa4382759
xie.ningning <1208813354@qq.com>
2019-05-09 16:10:59 +0800

    搭建项目工程

---
+++ src/main/java/com/springboot/blog/TomatoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomatoBlogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/springboot/blog/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.springboot.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "successful";
+    }
+}
